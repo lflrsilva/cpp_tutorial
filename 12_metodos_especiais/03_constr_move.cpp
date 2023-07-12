d08362bab43f1f39898e0b3f6f60de48dd936e2a
Luiz Fernando Lopes Rodrigues Silva <lflopes@eq.ufrj.br>
2023-07-12 18:25:51 -0300

    Atualizando tutorial sobre metodos especiais

--- 12_metodos_especiais/03_constr_move.cpp
+++ 12_metodos_especiais/03_constr_move.cpp
@@ -8,10 +8,53 @@
 
 Tópicos abordados
 -----------------
-  - Construtor por transferência de objetos
+  - Construtor por transferência (move) de objetos
 
-Construtores por transferência 
+Construtores por transferência
 ------------------------------
+  Os contrutores por transferência (move) são semelhantes a construção por
+  cópia. A diferença é que a transferência, de fato, transfere o conteúdo de
+  um objeto (fonte) para outro (destino). Com isso, a fonte perde
+  complementamente o acesso ao objeto, que agora pertence ao destino. Esta
+  condição se aplica quando a fonte é um objeto não nomeado.
+
+  Mas o que é um objeto não nomeado? Bem, são objetos temporários que não estão
+  associados a nada. Um exemplo de objeto não nomeado é o retorno de uma função.
+
+  Digamos que existe uma função que retorne o objeto minhaClasse.
+
+  minhaClasse fn();
+
+  Portanto, o retorno é apenas um objeto temporário e, para ser armazenado, deve
+  ser usado em uma construção ou atribuição. Este é o sentido do contrutor por
+  transferência.
+
+  minhaClasse ex1;          // construtor default
+  minhaClasse ex2 = ex1;    // construtor por cópia
+  minhaClasse ex3 = fn();   // construtor move
+
+  ex1 = ex2;                // atribuição por cópia
+  ex3 = minhaClasse();      // atribuição por move
+
+  As duas operações acima que usam move partem de objetos são temporários não
+  nomeados. Justamente por isso, a cópia não faz sentido e a transferência
+  (move) se aplica de forma mais eficiente.
+
+  A sintaxe de contrução e atribuição move usa como parâmetro a referência
+  rvalue da própria classe.
+
+  minhaClasse( minhaClasse&& );                // construção move
+  minhaClasse& operator= ( minhaClasse&& );    // atribuição move
+
+  Nos comandos acima, && se refere à referência a um rvalue (discutimos esse
+  assunto em aulas anteriores).
+
+  O conceito de transferência (move) se aplica melhor ainda em casos que usam
+  o operador new e delete. A memória alocada pelo operador new também é não
+  nomeada e, portanto, a trasferência para um objeto destino seria viável.
+
+  A maioria dos compiladores mais modernos conseguem otimizar a operação de 
+  retorno não nomeado. Nestes casos, a construção move nem seria usada.
 
 Referências
 -----------
@@ -30,12 +73,13 @@ class GeneralVector {
     // construtores e destrutor
     GeneralVector(int, double*);
     GeneralVector(int, double);
-    // construtor por cópia deep ou shallow (ver abaixo)
     GeneralVector(const GeneralVector& gv);
+    GeneralVector(const GeneralVector&& gv);           // move 
     ~GeneralVector() { delete[] data; }
 
     // sobrecarregamento de operadores
     GeneralVector& operator=(const GeneralVector&);
+    GeneralVector& operator=(const GeneralVector&&);  // move
     GeneralVector& operator+=(const GeneralVector&);
     GeneralVector& operator-=(const GeneralVector&);
     double& operator[](int i) const { return data[i]; }
@@ -64,15 +108,7 @@ GeneralVector::GeneralVector(int n, double value) {
   }
 }
 
-// Shallow copy
-
-GeneralVector::GeneralVector(const GeneralVector& gv) {
-  this->nSize = gv.nSize;
-  this->data = gv.data;
-}
-
 // Deep copy
-/*
 GeneralVector::GeneralVector(const GeneralVector& gv) {
   this->data = new double[this->nSize = gv.nSize];
 
@@ -80,7 +116,13 @@ GeneralVector::GeneralVector(const GeneralVector& gv) {
     this->data[i] = gv.data[i];
   }
 }
-*/
+
+// Move
+GeneralVector::GeneralVector(const GeneralVector&& gv) {
+  this->nSize = gv.nSize;
+  this->data = gv.data;
+}
+
 
 // sobrecarregamento de operadores
 GeneralVector& GeneralVector::operator=(const GeneralVector& vec) {
@@ -94,6 +136,13 @@ GeneralVector& GeneralVector::operator=(const GeneralVector& vec) {
       this->data[i] = vec[i];
     }
   }
+  return *this;
+}
+
+// atribuição move 
+GeneralVector& GeneralVector::operator=(const GeneralVector&& vec) {
+  this->nSize = vec.nSize;
+  this->data = vec.data;
   return *this;
 }
 
@@ -149,13 +198,10 @@ int main(int argc, char **argv) {
   // construção copiando elementos de v 
   GeneralVector vec1(size, v);
 
-  // construção por cópia
-  GeneralVector vec2(vec1);
+  // construção por move
+  GeneralVector vec2 = GeneralVector(vec1);
 
   std::cout << "Norma max de vec1 = " << vec1.maxnorm() << std::endl;
-
-  // testando sobrecarregamento =
-  // vec2 = vec1;
 
   std::cout << "Norma max de vec2 = " << vec2.maxnorm() << std::endl;
 
